8cb8b83947b8ad33cfa274d7ad25ef35d2b7f343
charles <cm3290006@gmail.com>
2023-03-17 17:34:26 +0300

    first commit

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+import Img1 from "../assets/portfolio-img1.png";
+import Img2 from "../assets/portfolio-img2.png";
+import Img3 from "../assets/portfolio-img3.png";
 
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <section className="section" id="work">
+      <div container mx-auto>
+        <div className="flex flex-col lg:flex-row gap-x-10 lg:gap-y-0 h-screen">
+          <div className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
+            <h2 className="h2 leading-tight text-accent">
+              My latest <br /> work.
+            </h2>
+            <p className="max-w-sm mb-16">
+              t is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsum is that it has a more-or-lP
+            </p>
+            <button className="btn btn-sm">View all projects</button>
+          </div>
+          <div className="group relative overflow-hidden border-2 border-white/20 rounded-*1">
+            <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+            <img
+              src={Img1}
+              alt=""
+              className="group-hover:scale-125 transition-all duration-500"
+            />
+            <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+              <span className="text-gradient"> UI/UX Design</span>
+            </div>
+            <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+              <span className="text-3*1 text-white">Project Title</span>
+            </div>
+          </div>
+          <div className="flex-1">
+            <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+            <img
+              src={Img1}
+              alt=""
+              className="group-hover:scale-125 transition-all duration-500"
+            />
+            {/* <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50"> */}
+              {/* <span className="text-gradient"> UI/UX Design</span> */}
+            {/* </div> */}
+            {/* <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50"> */}
+              {/* <span className="text-3*1 text-white">Project Title</span> */}
+            {/* </div> */}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Work;
